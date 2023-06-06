eed8fceafe1d40edd8011a8cd0554e11516f3ee0
Matsuoka0711 <taiki1544.0711@gmail.com>
2023-06-06 13:44:12 +0900

    catalogAdd.blade.phpの読み込みの設定変更前の保存

--- public/add/catalogAdd.blade.php
+++ public/add/catalogAdd.blade.php
@@ -6,17 +6,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/reset.css" rel="stylesheet">
   <link href="css/style.css" rel="stylesheet">
-  <title>Document</title>
+  <title>商品登録画面</title>
 </head>
 <body>
   <header>
       <div class="headerLogo">
-        <a href="http://localhost:8888/test2/public/catalog#">Sample Shop<span>-商品登録画面</span></a>
+        <a href="http://localhost:8888/test2/public/catalog#">Sample Shop</a>
       </div>
-      <div class="headerSearch">
+      <div class="headerNavi">
+        <a href="http://localhost:8888/test2/public/catalog#">商品一覧ページ</a>
+        <p>></p>
+        <p>商品登録画面</p>
+      </div>
     </header>
-
-    <a href="{{ remove('catalog') }}">商品画面一覧へ</a>
+    <div class="catalogAdd">
+      <table>
+        <label>商品画像</label>
+        
+      </table>
+    </div>
 
   </body>
 </html>

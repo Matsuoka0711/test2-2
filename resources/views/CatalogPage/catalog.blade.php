3a8a8f73c1ec57758d2a8cd5c95606bf25d9095c
Matsuoka0711 <taiki1544.0711@gmail.com>
2023-06-06 12:00:30 +0900

    catalogAdd 追加

--- resources/views/CatalogPage/catalog.blade.php
+++ resources/views/CatalogPage/catalog.blade.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <title>商品一覧ページ</title>
 </head>
@@ -22,7 +23,7 @@
   </header>
 
   <div class="itemAdd">
-    <a href="{{ url('/itemAdd/itemAdd.php') }}">商品登録</a>
+    <a href="{{ route('catalog') }}">商品登録</a>
   </div>
 
   <main>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/catalog', 'CatalogController@index')->name('catalog');
 
-Route::get('/catalog', function () {return view('CatalogPage.catalog');})->name('catalog');
 
 

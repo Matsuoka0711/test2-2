e1c0a7df6dd2eeef55f72ce554fe8bc75e0dae36
Matsuoka0711 <taiki1544.0711@gmail.com>
2023-11-29 22:13:59 +0900

    完成４

--- database/migrations/2023_11_21_001602_create_sales_table.php
+++ database/migrations/2023_11_21_001602_create_sales_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->timestamps();
         });
     }
